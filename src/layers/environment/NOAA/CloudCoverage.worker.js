296c248e32a36e4013714420b82ce2964b01ff84
Emeric Beaufays <emericbeaufays@gmail.com>
2024-09-17 17:49:18 +0200

    fix NOAA worker buffer context switch

--- src/layers/environment/NOAA/CloudCoverage.worker.js
+++ src/layers/environment/NOAA/CloudCoverage.worker.js
@@ -81,7 +81,7 @@ const CloudCoverageWorker = {
                         cloudsArrayBuffer2:cloudsArrayBuffer2,
                         dateBefore: e.data.dateBefore, 
                         dateAfter: e.data.dateAfter 
-                    }, [cloudsArrayBuffer1, cloudsArrayBuffer2, e.data.dateBefore, e.data.dateAfter]);
+                    }, [cloudsArrayBuffer1, cloudsArrayBuffer2]);
                 });
             }catch(error){
                 postMessage({error:error});
